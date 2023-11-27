7de5ecd86c06746d2bf7cc9bc89e0fdbca47a068
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2023-11-27 22:39:59 +0530

    Shortest Common Supersequence.cpp

--- Shortest Common Supersequence.cpp
+++ Shortest Common Supersequence.cpp
@@ -1,3 +1,14 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+
 class Solution
 {
     public:
@@ -14,5 +25,29 @@ class Solution
                }
            }
            return(m+n-dp[m][n]);
-      } 
+      } //code her
 };
+
+//{ Driver Code Starts.
+
+int main()
+{   
+    
+    int t;
+    
+    //taking total testcases
+    cin >> t;
+    while(t--){
+    string X, Y;
+    //taking String X and Y
+	cin >> X >> Y;
+	
+	//calling function shortestCommonSupersequence()
+	Solution obj;
+	cout << obj.shortestCommonSupersequence(X, Y, X.size(), Y.size())<< endl;
+    }
+	return 0;
+}
+
+
+// } Driver Code Ends
