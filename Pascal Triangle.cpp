334619ded2f19da219e92066e2c517f98309d6ed
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2023-11-28 13:24:59 +0530

    Create Pascal Triangle.cpp

---
+++ Pascal Triangle.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine().trim());
+
+            ArrayList<Long> ans = new Solution().nthRowOfPascalTriangle(n);
+            printAns(ans);
+        }
+    }
+    public static void printAns(ArrayList<Long> ans) {
+        for (Long x : ans) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    ArrayList<Long> nthRowOfPascalTriangle(int n) {
+        // code here
+    }
+}
