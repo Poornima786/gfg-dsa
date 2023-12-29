e709759276ed23ff5726cd2d383d57da640775da
Poornima Srivastava <152088344+Poornima786@users.noreply.github.com>
2023-12-29 13:20:02 +0530

    Create Check if a string is repetition of its substring of k-length.cpp

---
+++ Check if a string is repetition of its substring of k-length.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+	
+	int kSubstrConcat (int n, string s, int k)
+	{
+	    if(n % k)
+	        return 0;
+	        
+	    map<vector<int>, int> substr;
+	    
+	    vector<int> f(26, 0);
+	    
+	    for(int i = 0; i < n; i += k){
+	        for(int j = i - k; j < i and j > -1; j++)
+	            --f[s[j] - 'a'];
+	        for(int j = i; j < i + k; j++)
+	            ++f[s[j] - 'a'];
+	        
+	        substr[f]++;
+	        
+	        if(substr.size() > 2)
+	            return 0;
+	    }
+	    
+	    int freq = (*substr.begin()).second;
+	    
+	    return (freq == 1 or freq >= n / k - 1);
+
+	}
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+	{
+		int n; cin >> n;
+		string s; cin >> s;
+		int k; cin >> k;
+        Solution  ob;
+		cout << ob.kSubstrConcat (n, s, k) << endl;
+	}
+}
+// } Driver Code Ends
